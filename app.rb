6d00b06f9434828cb7f565ec53876694190affd0
Victor Kung <victor.y.kung@gmail.com>
2025-08-03 20:09:54 +0000

    Created RPS game, layout and view files. Also added Pico CSS.

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:root)
+end
+
+# If the user plays rock
+get("/rock") do
+
+  moves = ["rock", "paper", "scissors"]
+  @opponent_move = moves.sample
+
+  if @opponent_move == "rock"
+    @result = "tied"
+  elsif @opponent_move == "paper"
+    @result = "lost"
+  elsif @opponent_move == "scissors"
+    @result = "won"
+  end
+
+  erb(:rock)
+end
+
+# If the user plays paper
+get("/paper") do
+
+  moves = ["rock", "paper", "scissors"]
+  @opponent_move = moves.sample
+
+  if @opponent_move == "rock"
+    @result = "won"
+  elsif @opponent_move == "paper"
+    @result = "tied"
+  elsif @opponent_move == "scissors"
+    @result = "lost"
+  end
+
+  erb(:paper)
+end
+
+# If the user plays scissors
+get("/scissors") do
+
+  moves = ["rock", "paper", "scissors"]
+  @opponent_move = moves.sample
+
+  if @opponent_move == "rock"
+    @result = "lost"
+  elsif @opponent_move == "paper"
+    @result = "won"
+  elsif @opponent_move == "scissors"
+    @result = "tied"
+  end
+
+  erb(:scissors)
 end
